6768fc853acb804f8c562249c924132f2c193484
thanhbinh710 <thanhbinh710@gmail.com>
2017-07-10 06:58:56 +0800

    Back up before installing google-distance

--- public/src/components/explore_page.jsx
+++ public/src/components/explore_page.jsx
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Field, reduxForm } from 'redux-form';
 import { bindActionCreators } from 'redux';
 import { fetchLocations } from '../actions';
 import axios from 'axios';
@@ -16,63 +15,45 @@ class ExplorePage extends Component {
 	    super(props);
 
 	    this.state = {
-			formError: ""
-		}    
+	    	locationErr: "",
+			location: ""
+		}
+
+		this.populatePosition = this.populatePosition.bind(this); 
+		this.useCurrentLocation = this.useCurrentLocation.bind(this); 
   	}
 
   	componentDidMount() {
 		this.props.fetchLocations();
 	}
 
-  	// Render individual input field
-	renderInputField(field) {
-		const {meta : {touched, error} } = field;
-		const className =`form-group ${touched && error ? 'has-danger' : ''}`;
-		
-		return (
-			<div className={className}>
-				<input 
-					className="form-control"
-					type={field.type}
-					placeholder={field.placeholder}
-					{...field.input}
-				/>
-				<div className="error">
-					{touched ? error : ''}
-				</div>
-			</div>
-		);
+	onInputChange(location) {
+		this.setState({location});	
 	}
 
-	// renderMap() {
-		
-
-	// }
-
-	onSubmit(values) {
+	findNearestHawkerCenter() {
 		var { locations } = this.props;
-		// var distanceList = [];
-		// var service = new google.maps.DistanceMatrixService();
-				
-		// // console.log("locations", locations);
-		
+		console.log("location", this.state.location);
+		console.log("locations", locations);
+	}
 
-		// for (var i = 0; i < locations.length; i ++) {
-		// 	service.getDistanceMatrix({
-		// 		origins: `${values.location} Singapore`,
-  //               destinations: locations[i],
-  //               travelMode: "DRIVING"
-		// 	}, function(response) {
-		// 		console.log(response);
-		// 		if (response.status === 200) {
-		// 			distanceList.push({
-		// 				"hawker_name": locations[i].hawker_name,
-		// 				"hawker_location": locations[i].hawker_location,
-		// 				"duration": "12 hours 21 mins"
-		// 			});
-		// 		}
-		// 	})
-		// }
+	useCurrentLocation() {
+		if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(this.populatePosition);
+	    } else {
+	        this.setState({locationErr: "Geolocation is not supported by this browser."});
+	    }
+	}
+
+	populatePosition(position) {
+	    var currentLoc = `${position.coords.latitude} ${position.coords.longitude}`;
+	    console.log("currentLoc", currentLoc);
+
+	    // Unable to bind 'this' with the timeout function
+	    setTimeout(function() {
+	    	// Error setting state
+    		this.setState({location: currentLoc});
+    	}, 500);
 
 	}
 
@@ -86,20 +67,22 @@ class ExplorePage extends Component {
 
 				<div className="row justify-content-center">
 					<div className="col-10 col-md-8 col-lg-5">
-						<div className="error form-error">{this.state.formError}</div>
-							
-						<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-							<p>Please key in your location</p>
-							<Field 
-								placeholder="Your location"
-								name="location"
-								type="text"
-								component={this.renderInputField}
-							/>
+						<div className="error form-error">{this.state.locationErr}</div>
+						<div className="input_box">
+						  	<input 
+						  		className="form-control input_full" 
+						  		type="text" 
+						  		placeholder="Your location..." 
+						  		value={this.state.location}
+						  		onChange={event => this.onInputChange(event.target.value)} />
+						</div>
 
-							<button action="submit" className="btn btn-find">Find A Hawker</button>
+						<div className="button-container">
+							<button className="btn-location" onClick={()=> this.useCurrentLocation()}>Use My Location</button>
+							<button className="btn btn-find" onClick={()=> this.findNearestHawkerCenter()}>Find A Hawker</button>
+						</div>
 
-						</form>
+						
 
 					</div>
 				</div>
@@ -108,19 +91,7 @@ class ExplorePage extends Component {
     }
 }
 
-// Validation methods for the input values
-function validate(values) {
-	const errors = {};
 
-	if(!values.location) {
-		errors.location = "Please enter your location";
-	} else {
-		if(values.location.length < 3) errors.location = "Please include at least 3 characters of your location";
-		if (/[\[\]\\/{}|\\<>]/.test(values.location) == true) {errors.location = "Please obmit any special character from your location.";}   
-	}
-
-	return errors;
-}
 
 function mapStateToProps(state) {
 	return {
@@ -132,11 +103,6 @@ function mapDispatchToProps(dispatch) {
 	return bindActionCreators({ fetchLocations }, dispatch);
 }
 
-export default reduxForm({
-  	validate,
-	form: 'ExploreForm'
-})(
-	connect(mapStateToProps, mapDispatchToProps)(ExplorePage)
-);
 
 
+export default connect(mapStateToProps, mapDispatchToProps)(ExplorePage);
